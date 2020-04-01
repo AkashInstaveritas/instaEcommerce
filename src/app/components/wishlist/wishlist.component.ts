15e97a9233fd923c3c3c98ae8adc8dfac017098d
Akash Singh <singh9800akash@gmail.com>
2020-04-02 00:57:53 +0530

    Wishlist Remove Product Completed on 01/04/2020

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -12,13 +12,28 @@ export class WishlistComponent implements OnInit {
   constructor(private _wishlist: WishlistService) { }
 
   public wishlists = [];
+  showMsg: boolean = false;
 
 
   ngOnInit(): void {
+    this.getWishlist();
+  }
+
+  getWishlist()
+  {
     this._wishlist.getWishlist()
     .subscribe(data => this.wishlists = data);
   }
 
+  removeProduct(event: MouseEvent, id)
+  {
+    event.preventDefault();
 
+    this._wishlist.removeProductFromWishlist(id)
+    .subscribe(data => {
+      this.showMsg= true,
+      this.getWishlist()
+    });
+  }
 
 }
--- src/app/services/wishlist.service.ts
+++ src/app/services/wishlist.service.ts
@@ -10,14 +10,25 @@ export class WishlistService {
 
   constructor(private _http: HttpClient, private Token: TokenService) { }
 
-  _url = 'http://127.0.0.1:81/api/wishlist';
+  private baseUrl = 'http://127.0.0.1:81/api';
+
 
   getWishlist(): Observable<any>
   {
-    return this._http.get<any>(this._url, {
+    return this._http.get<any>(`${this.baseUrl}/wishlist`, {
       headers: new HttpHeaders({
         Authorization: `Bearer ${this.Token.get()}`
       })
     });
   }
+
+  removeProductFromWishlist(id): Observable<any>
+  {
+    return this._http.delete<any>(`${this.baseUrl}/wishlist/product/` + id, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.Token.get()}`
+      })
+    });
+  }
+
 }
